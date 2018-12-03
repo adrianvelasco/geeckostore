508c276548ef3cd5472917e6d2fccf0262b4d12c
adrianvelasco <adrian_velascocuriel@hotmail.com>
2018-12-02 21:32:44 -0700

    cuarta subida

--- src/Login/Login.jsx
+++ src/Login/Login.jsx
@@ -1,68 +1,68 @@
-import React, {Component} from 'react';
-import "../css/Login.css";
-class Login extends Component {
+import React from 'react';
+import PropTypes from 'prop-types';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import FormControl from '@material-ui/core/FormControl';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Input from '@material-ui/core/Input';
+import InputLabel from '@material-ui/core/InputLabel';
+import LockIcon from '@material-ui/icons/LockOutlined';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import withStyles from '@material-ui/core/styles/withStyles';
+import {styles} from '../styles/Login';
+import '../css/Login.css';
+
+class Login extends React.Component{
 
   render(){
+    const { classes } = this.props;
 
-    let content = (
-      <div className="login-wrap">
-        <div className="login-html">
-          <input id="tab-1" type="radio" name="tab" className="sign-in" checked/><label htmlFor="tab-1" className="tab">Iniciar</label>
-          <input id="tab-2" type="radio" name="tab" className="sign-up" checked/><label htmlFor="tab-2" className="tab">Registrate</label>
-          <div className="login-form">
-            <div className="sign-in-htm">
-              <div className="group">
-                <label htmlFor="user" className="label">Usuario</label>
-                <input id="user" type="text" className="input"/>
-              </div>
-              <div className="group">
-                <label htmlFor="pass" className="label">Contraseña</label>
-                <input id="pass" type="password" className="input" data-type="password" />
-              </div>
-              <div className="group">
-                <input id="check" type="checkbox" className="check" checked/>
-                <label htmlFor="check"><span className="icon"></span>Recuerdame</label>
-              </div>
-              <div className="group">
-                <input type="submit" className="button" value="Iniciar Sesión"/>
-              </div>
-              <div className="hr"/>
-              <div className="foot-link">
-                <a href="/">Olvidaste tu Contraseña?</a>
-              </div>
-            </div>
-            <div className="sign-up-htm">
-              <div className="group">
-                <label htmlFor="user" className="label">Usuario</label>
-                <input id="user" type="text" className="input"/>
-              </div>
-              <div className="group">
-                <label htmlFor="pass" className="label">Contraseña</label>
-                <input id="pass" type="password" className="input" data-type="password"/>
-              </div>
-              <div className="group">
-                <label htmlFor="pass" className="label">Repita la Contraseña</label>
-					      <input id="pass" type="password" className="input" data-type="password" />
-              </div>
-              <div className="group">
-                <label htmlFor="pass" className="label">Correo Electronico</label>
-                <input id="pass" type="text" className="input" />
-              </div>
-              <div className="group">
-                <input type="submit" className="button" value="Registrate" />
-              </div>
-              <div className="hr"></div>
-              <div className="foot-lnk">
-                <label htmlFor="tab-1" /><a href="/">Ya es Miembro??</a>
-              </div>
-            </div>
-          </div>
-        </div>
+    return (
+      <div className="fondo">
+        <main className={classes.main}>
+          <CssBaseline />
+          <Paper className={classes.paper}>
+            <Avatar className={classes.avatar}>
+              <LockIcon />
+            </Avatar>
+            <Typography component="h1" variant="h5">
+              Iniciar Sesión
+            </Typography>
+            <form className={classes.form}>
+              <FormControl margin="normal" required fullWidth>
+                <InputLabel htmlFor="email">Usuario</InputLabel>
+                <Input id="email" name="email" autoComplete="email" autoFocus />
+              </FormControl>
+              <FormControl margin="normal" required fullWidth>
+                <InputLabel htmlFor="password">Contraseña</InputLabel>
+                <Input name="password" type="password" id="password" autoComplete="current-password" />
+              </FormControl>
+              <FormControlLabel
+                control={<Checkbox value="remember" color="primary" />}
+                label="Recuerdame"
+              />
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                color="primary"
+                className={classes.submit}
+              >
+                Continuar
+              </Button>
+            </form>
+          </Paper>
+        </main>
       </div>
     );
-
-    return content;
   }
 }
 
-export default Login;
+Login.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(Login);
