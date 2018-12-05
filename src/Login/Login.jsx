ede4dccfa638c76f6605a8781ec9fe2c777bf41c
adrianvelasco <adrian_velascocuriel@hotmail.com>
2018-12-04 20:02:26 -0700

    septima subida

--- src/Login/Login.jsx
+++ src/Login/Login.jsx
@@ -1,16 +1,25 @@
 import React, {Component} from 'react';
+import PropTypes from 'prop-types'
 import {styles} from '../styles/Login';
-
+import CssBaseline from '@material-ui/core/CssBaseline';
+import withStyles from '@material-ui/core/styles/withStyles';
 class Login extends Component{
 
   render(){
 
-    const = {classes} = this.props;
+    const {classes} = this.props;
 
     return(
       <div>
+        <main className={classes.main}>
 
+        </main>
       </div>
     );
   }
 }
+Login.propTypes = {
+  classes: PropTypes.object.isRequired,
+}
+
+export default withStyles(styles)(Login);
