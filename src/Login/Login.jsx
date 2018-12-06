bac2d4ac5a8423795e453addf8f74c12e5971425
adrianvelasco <adrian_velascocuriel@hotmail.com>
2018-12-05 18:53:52 -0700

    octava subida

--- src/Login/Login.jsx
+++ src/Login/Login.jsx
@@ -1,8 +1,19 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types'
 import {styles} from '../styles/Login';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import Input from '@material-ui/core/Input';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import Checkbox from '@material-ui/core/Checkbox';
+import TextField from '@material-ui/core/TextField';
+import InputLabel from '@material-ui/core/InputLabel';
+import Typography from '@material-ui/core/Typography';
 import CssBaseline from '@material-ui/core/CssBaseline';
+import FormControl from '@material-ui/core/FormControl';
 import withStyles from '@material-ui/core/styles/withStyles';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
 class Login extends Component{
 
   render(){
@@ -12,7 +23,21 @@ class Login extends Component{
     return(
       <div>
         <main className={classes.main}>
-
+          <CssBaseline />
+          <Paper className={classes.paper}>
+             <Avatar className={classes.avatar}>
+              <i className="fas fa-user"></i>
+             </Avatar>
+             <Typography component="h1" variant="h5">
+               INICIAR SESIÓN
+             </Typography>
+             <form className={classes.form}>
+               <FormControl margin="normal" required fullWidth>
+                 <InputLabel htmlFor="email">Email Address</InputLabel>
+                 <Input id="email" name="email" autoComplete="email" autoFocus />
+              </FormControl>
+             </form>
+          </Paper>
         </main>
       </div>
     );
