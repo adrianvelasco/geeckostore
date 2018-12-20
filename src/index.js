930bb4092321ac8ce32f4390afdb9fe830388da3
adrianvelasco <adrian_velascocuriel@hotmail.com>
2018-12-19 22:59:33 -0700

    subida

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './css/index.css';
-import 'react-mdl/extra/material.css';
-import 'react-mdl/extra/material.js';
 import Routerstatic from './Route/RutesStatic.js';
 import {BrowserRouter} from 'react-router-dom';
 import * as serviceWorker from './serviceWorker/serviceWorker.js';
